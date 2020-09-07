bec45bb893ef124a887959362f553912c2feede9
Guilherme Peixoto <guilherme_peixoto_@hotmail.com>
2020-09-07 14:52:29 -0300

    include handle exception if not found log file in show route

--- src/services/GetGameByIdService.ts
+++ src/services/GetGameByIdService.ts
@@ -1,3 +1,8 @@
+import fs from 'fs';
+
+import AppError from '@shared/errors/AppError';
+import fileConfig from '@config/file';
+
 import Game from '@models/Game';
 import IGameRepository from '@repositories/contracts/IGameRepository';
 
@@ -5,6 +10,10 @@ export default class GetGameByIdService {
   constructor(private gameRepository: IGameRepository) {}
 
   public async run(id: number): Promise<Game | undefined> {
+    if (!fs.existsSync(`${fileConfig.filePath}\\${fileConfig.fileName}`)) {
+      throw new AppError('games.log file not found.', 404);
+    }
+
     return this.gameRepository.findOne(id);
   }
 }
